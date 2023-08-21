6c77f84f289866e1d8567b9043e9f0ae81246e8e
hello123-vn <giadat547@gmail.com>
2023-08-21 11:47:02 +0700

    FILE MỚI

---
+++ src/MAIN_COMPONENT/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './About.css'
+
+function NavBar() {
+    return (
+        <>
+            <nav className='nav'>
+                <div>
+                    <ul className='navbar'>
+                        <li>Học Phí</li>
+                        <li>Giáo Viên</li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
